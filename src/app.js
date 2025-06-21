6fc8134c6a3bff6a771a170f76f295a32b2513a3
tusharbaskheti18 <baskhetitushar18@gmail.com>
2025-06-21 21:31:06 +0530

    add search routes to app

--- src/app.js
+++ src/app.js
@@ -21,6 +21,7 @@ import subscriptionRouter from "./routes/subscription.routes.js"
 import videoRouter from "./routes/video.routes.js"
 import commentRouter from "./routes/comment.routes.js"
 import playlistRouter from "./routes/playlist.routes.js"
+import searchRouter from "./routes/searching.routes.js";
 
 //routes declaration
 app.use("/api/v1/users", userRouter)
@@ -28,5 +29,6 @@ app.use("/api/v1/subscriptions", subscriptionRouter)
 app.use("/api/v1/videos", videoRouter)
 app.use("/api/v1/comments", commentRouter)
 app.use("/api/v1/playlist", playlistRouter)
+app.use("/api/v1/search", searchRouter);
 
 export { app }
